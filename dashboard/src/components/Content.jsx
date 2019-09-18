fad59f4e7b56c4451389f961920b58e23a123834
Arjun Gandhi <arjungandhi06@gmail.com>
2019-09-18 08:37:11 -0400

    added basic react structure + menu

---
+++ dashboard/src/components/Content.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+
+export default class Content extends React.Component {
+
+  constructor(props) {
+    //make a blank articles list in state
+    super(props)
+  }
+
+  componentDidMount() { }
+
+  render() {
+    return (<Switch>
+      
+
+
+    </Switch>)
+  }
+}
